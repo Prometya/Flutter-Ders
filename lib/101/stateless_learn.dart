5eed7034ab73584851181fd43a041445346f984a
Prometya <pictub@gmail.com>
2022-11-18 18:36:15 +0300

    4. video başladım 18.11.22 18:04

---
+++ lib/101/stateless_learn.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class StatelessLearnView extends StatelessWidget {
+  const StatelessLearnView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Stateless Learn'),
+        ),
+        body: Column(
+          children: <Widget>[
+            ErayTitleTextWidget(
+              metin:
+                  "Ali", //Metin yazdık ve custom widget ımızdan uygulamada göstermiş olduk
+            ),
+            ErayTitleTextWidget(
+              metin: "Veli",
+            ),
+            ErayTitleTextWidget(
+              metin: "4950",
+            ),
+          ],
+        ));
+  }
+}
+
+class ErayTitleTextWidget extends StatelessWidget {
+  //Kendi widgetımızı oluşturduk
+  const ErayTitleTextWidget({Key? key, required this.metin})
+      : super(key: key); //Nullable olmaması için required kullandık
+  final String metin; // atanması gereken metin olduğunu belirttik
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      metin, //metin i de buna istinaden aldı
+      style: Theme.of(context).textTheme.headlineLarge,
+    );
+  }
+}
